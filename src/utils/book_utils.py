5521fec4eae41f3e3d798fda287f15f1c7a234df
Meng Guozao <meng.guozao@core42.ai>
2024-10-14 13:06:35 +0400

    remove page 0 for books

--- src/utils/book_utils.py
+++ src/utils/book_utils.py
@@ -44,7 +44,7 @@ def get_chapters(book: Book) -> List[Chapter]:
 def get_prev_next_chapter_urls(book: Book, chapter_no: int) -> (str, str):
     prev_chapter = chapter_no - 1
     next_chapter = chapter_no + 1
-    prev_chapter_url = generate_chapter_url(book.slug, prev_chapter) if prev_chapter >= 0 else None
+    prev_chapter_url = generate_chapter_url(book.slug, prev_chapter) if prev_chapter >= 1 else None
     next_chapter_url = generate_chapter_url(book.slug, next_chapter) if next_chapter <= book.chapter_number else None
     return prev_chapter_url, next_chapter_url
 
